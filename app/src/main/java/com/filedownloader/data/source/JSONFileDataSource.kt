101e22eb9a9f892afea57a570b371932eecd9202
mo_fawzy <mohamed.fawzy@algorithmz.net>
2021-06-04 00:24:09 +0200

    add application name to manifest

--- app/src/main/java/com/filedownloader/data/source/JSONFileDataSource.kt
+++ app/src/main/java/com/filedownloader/data/source/JSONFileDataSource.kt
@@ -4,28 +4,25 @@ import android.app.Application
 import android.util.Log
 import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStream
 import java.io.InputStreamReader
 import javax.inject.Inject
 
 class JSONFileDataSource @Inject constructor(private val application: Application) {
 
     fun readFile(fileName: String) {
-        var reader: BufferedReader? = null
-        reader = BufferedReader(
-            InputStreamReader(application.assets.open(fileName), "UTF-8")
-        )
-
-        var word: String? = ""
-        val stringBuilder = StringBuilder()
-        while (true) {
-            try {
-                if (reader.readLine().also { word = it } == null) break
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            stringBuilder.append(word)
+        var data: String? = ""
+        try {
+            val stream: InputStream = application.assets.open(fileName)
+            val size: Int = stream.available()
+            val buffer = ByteArray(size)
+            stream.read(buffer)
+            stream.close()
+            data = String(buffer)
+            Log.i("JSONFileDataSource", data.toString())
+        } catch (e: IOException) {
+            Log.i("JSONFileDataSource", e.localizedMessage)
         }
 
-        Log.i("JSONFileDataSource", stringBuilder.toString())
     }
 }
