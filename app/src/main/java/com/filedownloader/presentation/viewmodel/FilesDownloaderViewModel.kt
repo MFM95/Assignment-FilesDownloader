46fcc3886067e72a30ff63b73c82d8d0d1d588cf
mo_fawzy <mohamed.fawzy@algorithmz.net>
2021-06-07 10:54:34 +0200

    refactor code, move download logic to the view model

--- app/src/main/java/com/filedownloader/presentation/viewmodel/FilesDownloaderViewModel.kt
+++ app/src/main/java/com/filedownloader/presentation/viewmodel/FilesDownloaderViewModel.kt
@@ -1,10 +1,15 @@
 package com.filedownloader.presentation.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import com.downloader.OnDownloadListener
+import com.downloader.PRDownloader
 import com.filedownloader.core.BaseViewModel
 import com.filedownloader.data.source.model.FileItem
 import com.filedownloader.domain.interactor.ParseJsonFileUseCase
+import com.filedownloader.domain.interactor.PreferencesUseCase
 import com.filedownloader.domain.interactor.ReadFileUseCase
+import com.filedownloader.presentation.uimodel.DownloadState
+import com.filedownloader.presentation.view.adapter.FilesAdapter
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.addTo
@@ -17,14 +22,15 @@ import kotlin.collections.ArrayList
 
 class FilesDownloaderViewModel @Inject constructor(
     private val readFileUseCase: ReadFileUseCase,
-    private val parseJsonFileUseCase: ParseJsonFileUseCase
+    private val parseJsonFileUseCase: ParseJsonFileUseCase,
+    private val preferencesUseCase: PreferencesUseCase
 ) :
     BaseViewModel() {
 
     val filesLiveData by lazy { MutableLiveData<ArrayList<FileItem>>() }
     val errorLiveData by lazy { MutableLiveData<Throwable>() }
-
     val selectedFiles by lazy { MutableLiveData<ArrayList<Long>>() }
+    lateinit var filesAdapter: FilesAdapter
 
     fun readFile(fileName: String) {
         readFileUseCase.readFile(fileName)
@@ -43,40 +49,79 @@ class FilesDownloaderViewModel @Inject constructor(
             .addTo(compositeDisposable)
     }
 
+    fun startDownload(url: String, dirPath: String, fileName: String, position: Int) {
+        if (filesAdapter.items[position].downloadState == DownloadState.COMPLETED
+            || filesAdapter.items[position].downloadState == DownloadState.DOWNLOADING
+            || filesAdapter.items[position].downloadState == DownloadState.PENDING
+        )
+            return
+        updateItemState(position, DownloadState.PENDING)
+        filesAdapter.items[position].downloadId =
+            PRDownloader.download(url, dirPath, fileName)
+                .build()
+                .setOnStartOrResumeListener {
+                    updateItemState(position, DownloadState.DOWNLOADING)
+                }
+                .setOnPauseListener {
+                    filesAdapter.items[position].numberOfFailures = 0
+                    updateItemState(position, DownloadState.NORMAL)
+
+                }
+                .setOnCancelListener {
+                    filesAdapter.items[position].numberOfFailures = 0
+                    updateItemState(position, DownloadState.NORMAL)
+                }
+                .setOnProgressListener {
+                    var progress = (it.currentBytes * 100) / it.totalBytes
+                    if (it.totalBytes < 0L) {
+                        progress = (it.currentBytes * 100L) / 10000000
+                    }
+                    updateItemState(position, DownloadState.DOWNLOADING, progress)
+                }
+                .start(object : OnDownloadListener {
+                    override fun onDownloadComplete() {
+                        filesAdapter.items[position].numberOfFailures = 0
+                        updateItemState(position, DownloadState.COMPLETED)
+                        saveDownloadedFileID(filesAdapter.items[position].fileItem.id.toString())
+//                        openFile(filesAdapter.items[position].fileItem.name, filesAdapter.items[position].fileItem.type)
+                    }
+
+                    override fun onError(error: com.downloader.Error?) {
+                        handleErrors(url, dirPath, fileName, position)
+                    }
+                })
+    }
+
+    private fun updateItemState(position: Int, state: DownloadState, progress: Long? = null) {
+        filesAdapter.items[position].downloadState = state
+        progress?.let {
+            if (progress in 0..100) {
+                filesAdapter.items[position].downloadProgress = progress.toInt()
+            }
+        }
+        filesAdapter.notifyItemChanged(position)
+    }
+
+    private fun handleErrors(url: String, dirPath: String, fileName: String, position: Int) {
+        /*** Retry 3 times then display error state **/
+        if (filesAdapter.items[position].numberOfFailures < 4) {
+            filesAdapter.items[position].numberOfFailures++
+            filesAdapter.items[position].downloadState =
+                DownloadState.NORMAL
+            startDownload(url, dirPath, fileName, position)
+        } else {
+            filesAdapter.items[position].numberOfFailures = 0
+            updateItemState(position, DownloadState.FAILED)
+        }
+    }
 
 
-//    fun download(url: String, file: File): Observable<Int> {
-//        val okHttpBuilder = okHttpClient.newBuilder()
-//            .connectTimeout(HTTP_TIMEOUT.toLong(), TimeUnit.SECONDS)
-//            .readTimeout(HTTP_TIMEOUT.toLong(), TimeUnit.SECONDS)
-//        this.okHttpClient = okHttpBuilder.build()
-//        return Observable.create<Int> { emitter ->
-//            val request = Request.Builder().url(url).build()
-//            val response = okHttpClient.newCall(request).execute()
-//            val body = response.body
-//            val responseCode = response.code
-//            if (responseCode >= HttpURLConnection.HTTP_OK &&
-//                responseCode < HttpURLConnection.HTTP_MULT_CHOICE &&
-//                body != null) {
-//                val length = body.contentLength()
-//                body.byteStream().apply {
-//                    file.outputStream().use { fileOut ->
-//                        var bytesCopied = 0
-//                        val buffer = ByteArray(BUFFER_LENGTH_BYTES)
-//                        var bytes = read(buffer)
-//                        while (bytes >= 0) {
-//                            fileOut.write(buffer, 0, bytes)
-//                            bytesCopied += bytes
-//                            bytes = read(buffer)
-//                            emitter.onNext(
-//                                ((bytesCopied * 100)/length).toInt())
-//                        }
-//                    }
-//                    emitter.onComplete()
-//                }
-//            } else {
-//                // Report the error
-//            }
-//        }
-//    }
+    fun saveDownloadedFileID(id: String) {
+        preferencesUseCase.saveDownloadedFileID(id)
+    }
+
+    fun isFileDownloaded(id: String): Boolean {
+        return preferencesUseCase.isFileDownloaded(id)
+    }
+
 }
